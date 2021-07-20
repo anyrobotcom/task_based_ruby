ab219c29f7fd6a18421631135ef5617e1fcd3eab
Kuba <ouykjh@DESKTOP-4BCRFCI.localdomain>
2021-07-20 08:44:59 +0200

    fix attachments add validation test

--- job_results_success_all_attachments.rb
+++ job_results_success_all_attachments.rb
@@ -16,9 +16,11 @@ response = Faraday.post(payload['results_url']) do |req|
   req.body = { genre: :success, result: { color: :green }, attachments: [
     { genre: :text, name: 'list.txt', code: :important_stuff, body: 'ok' },
     { genre: :html, name: 'list.htm', code: :important_stuff, body: '<p>ok</p>' },
-    { genre: :file, name: 'list.png', code: :important_stuff, body: 'data:image/png;base64,...' },
+    { genre: :file, name: 'list.png', code: :important_stuff, body: 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
+    AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
+        9TXL0Y4OHwAAAABJRU5ErkJggg==' },
     { genre: :table, name: 'xls.csv', code: :important_stuff, body: 'a,b,c;1,2,3' },
-    { genre: :kpi, name: 'stats.kpi', code: :important_stuff, body: { genre: :integer, value: 123 } },
+    { genre: :kpi, name: 'stats.kpi', code: :important_stuff, body: { genre: :integer, value: 123, target: 1000 }.to_json },
   ] }.to_json
 end
 
