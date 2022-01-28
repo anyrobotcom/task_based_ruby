7d48e6efb34b0354671f5ca34adf73e243c66e54
Piotr Żurek <piotr.zurek@unitedideas.pl>
2022-01-28 16:22:42 +0100

    Fix Hello world in Python

--- tasks/hello_world.py
+++ tasks/hello_world.py
@@ -4,21 +4,31 @@ import os
 import sys
 from pprint import pprint
 
+def safeprint(s):
+  try:
+    print(s)
+  except UnicodeEncodeError:
+    if sys.version_info >= (3,):
+      print(s.encode('utf8').decode(sys.stdout.encoding))
+    else:
+      print(s.encode('utf8'))
+
+
 # Test output with
 
-print("Hello, I'm AnyRobot! 😀")
+safeprint("Hello, I'm AnyRobot! 😀")
 
 # Test Chromedriver (remember to install it first)
 
 chromedriver_result = os.popen("chromedriver --version").read()
 
 if chromedriver_result.count("ChromeDriver") > 0:
-  print("Chromedriver is present ✅")
+  safeprint("Chromedriver is present ✅")
 else:
-  print("No chromedriver present, aborting 🛑")
+  safeprint("No chromedriver present, aborting 🛑")
   sys.exit(1) # Status code > 0 -> problem
 
 # Input ARGV + ENV
 
-print("Arguments: %s" % sys.argv)
-print("Environment: %s" % os.environ)
+safeprint("Arguments: %s" % sys.argv)
+safeprint("Environment: %s" % os.environ)
