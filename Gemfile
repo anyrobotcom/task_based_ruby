7dc8ff1c84a7a0548ad8f43f18c089ec4a701cff
Piotr Żurek <piotr.zurek@unitedideas.pl>
2022-02-15 15:31:00 +0100

    Implement advanced test

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 source "https://rubygems.org"
 gem "faraday"
-gem 'mini_magick', '4.11.0'
+gem "mini_magick", "4.11.0"
 gem "rake"
-gem 'ostruct'
-gem 'ffi'
-gem 'webdrivers'
+gem "ostruct"
+gem "ffi"
+gem "webdrivers"
+gem "rubyXL"
+gem "watir"
 
 group :development do
   gem "solargraph"
---
+++ tasks/advanced_test.rb
@@ -0,0 +1,59 @@
+require 'rubyXL'
+require 'rubyXL/convenience_methods'
+require 'watir'
+
+$stdout.sync = true
+
+puts 'Testing Excel loading...'
+
+workbook = RubyXL::Parser.parse('../assets/test_sheet.xlsx')
+
+puts 'Excel loaded!'
+
+val = workbook[0][0][0].value
+
+puts val
+
+puts 'Testing browser...'
+
+chrome_options = {
+  prefs: {
+    download: {
+      'prompt_for_download' => false,
+      'directory_upgrade' => true,
+    },
+    'profile' => {
+      'default_content_setting_values' => { 'automatic_downloads' => 1 },
+    }
+  },
+}
+
+browser = Watir::Browser.new(:chrome, headless: false, options: chrome_options)
+
+browser.window.maximize
+
+Selenium::WebDriver.logger.level = :error
+
+browser.goto('https://www.google.com')
+
+sleep 5
+
+ok_button = browser.div(role: 'dialog').buttons[3]
+
+ok_button.click
+
+sleep 1
+
+search_input = browser.input(name: 'q')
+
+search_input.send_keys(val)
+
+search_input.send_keys(:enter)
+
+Watir::Wait.until(timeout: 30) { browser.div(id: 'top_nav').present? }
+
+sleep 5
+
+browser.close
+
+puts 'Browser test completed!'
