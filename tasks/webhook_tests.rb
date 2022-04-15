4c8b61eff62cd72eebed32310480888b8e7ff722
Lukasz Chojnowski <lukasz.chojnowski@unitedideas.pl>
2022-04-15 11:59:34 +0200

    Added webhooks

---
+++ tasks/webhook_tests.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+# TODO: to jest test tej funkcji https://unitedideas.atlassian.net/wiki/spaces/ANY/pages/2168455505/Webhooks
+
+# TRzeba do workflow w orkiestratorze i dodać webhook jako trigger
