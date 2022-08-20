cc2a5d854173c4026bb4eca0eed6d4c2ea80a320
Tomasz Gawlik <tomasz.gawlik@bivrost.pro>
2022-08-20 17:46:19 +0200

    Restored emoji characters

--- tasks/hello_world.py
+++ tasks/hello_world.py
@@ -17,16 +17,16 @@ def safeprint(s):
 
 # Test output with
 
-safeprint("Hello, I'm AnyRobot! :)")
+safeprint("Hello, I'm AnyRobot! 😀")
 
 # Test Chromedriver (remember to install it first)
 
 chromedriver_result = os.popen("chromedriver --version").read()
 
 if chromedriver_result.count("ChromeDriver") > 0:
-  safeprint("Chromedriver is present OK")
+  safeprint("Chromedriver is present ✅")
 else:
-  safeprint("No chromedriver present, aborting STOP")
+  safeprint("No chromedriver present, aborting 🛑")
   sys.exit(1) # Status code > 0 -> problem
 
 # Input ARGV + ENV
