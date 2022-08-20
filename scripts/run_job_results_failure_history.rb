75d0932fc03a3be3ef0fa2df8174eabd619bc673
Tomasz Gawlik <tomasz.gawlik@bivrost.pro>
2022-08-20 16:26:43 +0200

    Windows cmd support

--- scripts/run_job_results_failure_history.rb
+++ scripts/run_job_results_failure_history.rb
@@ -13,16 +13,43 @@ def run_command_with_live_logging(cmd)
 end
 
 # --- START OF DEBUG ---
-puts "---------- SCRIPT DETAILS"
-puts "RUNNING FILE: #{__dir__ + "/" + __FILE__}"
-if File.exist?("input/payload.json")
-  puts "---------- PAYLOAD"
-  puts `cat input/payload.json`
-else
-  puts "PAYLOAD: NONE"
+class Platform
+  def self.mac
+    success = (/darwin/ =~ RUBY_PLATFORM) != nil
+  rescue StandardError => e
+    puts 'error checking for mac'
+  end
+
+  def self.win
+    success = (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  rescue StandardError => e
+    puts 'error checking for windows'
+  end
 end
-puts "---------- GIT UPDATE"
-puts `cd ~/Code/runnertests && git pull`
+
+if Platform.win
+  puts "---------- SCRIPT DETAILS"
+  puts "RUNNING FILE: #{__dir__ + "/" + __FILE__}"
+  if File.exist?("input/payload.json")
+    puts "---------- PAYLOAD"
+    puts `type input\\payload.json`
+  else
+    puts "PAYLOAD: NONE"
+  end
+  puts "---------- GIT UPDATE"
+  puts `cd %UserProfile%/Code/runnertests && git pull`
+elsif Platform.mac
+  puts "---------- SCRIPT DETAILS"
+  puts "RUNNING FILE: #{__dir__ + "/" + __FILE__}"
+  if File.exist?("input/payload.json")
+    puts "---------- PAYLOAD"
+    puts `cat input/payload.json`
+  else
+    puts "PAYLOAD: NONE"
+  end
+  puts "---------- GIT UPDATE"
+  puts `cd ~/Code/runnertests && git pull`
+end
 # --- END OF DEBUG ---
 
 run_command_with_live_logging("cd \"#{ENV['HOME']}/Code/runnertests/tasks\" && bundle exec ruby job_results_failure_history.rb \"#{Dir.pwd}\"")
