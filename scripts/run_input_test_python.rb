745af95d9243be4b89c72a01b2b9161097d1ba99
Tomasz Gawlik <tomasz.gawlik@bivrost.pro>
2022-08-21 23:33:23 +0200

    Add input test

---
+++ scripts/run_input_test_python.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+$stdout.sync = true
+def run_command_with_live_logging(cmd)
+  IO.popen("#{cmd} 2>&1") do |pipe|
+    pipe.sync = true
+    while (str = pipe.gets)
+      puts str
+    end
+  end
+end
+
+# --- START OF DEBUG ---
+class Platform
+  def self.mac
+    success = (/darwin/ =~ RUBY_PLATFORM) != nil
+  rescue StandardError => e
+    puts 'error checking for mac'
+  end
+
+  def self.win
+    success = (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  rescue StandardError => e
+    puts 'error checking for windows'
+  end
+end
+
+if Platform.win
+  puts "---------- SCRIPT DETAILS"
+  puts "RUNNING FILE: #{__dir__ + "/" + __FILE__}"
+  if File.exist?("input/payload.json")
+    puts "---------- PAYLOAD"
+    puts `type input\\payload.json`
+  else
+    puts "PAYLOAD: NONE"
+  end
+  puts "---------- GIT UPDATE"
+  puts `cd %UserProfile%/Code/runnertests && git pull`
+elsif Platform.mac
+  puts "---------- SCRIPT DETAILS"
+  puts "RUNNING FILE: #{__dir__ + "/" + __FILE__}"
+  if File.exist?("input/payload.json")
+    puts "---------- PAYLOAD"
+    puts `cat input/payload.json`
+  else
+    puts "PAYLOAD: NONE"
+  end
+  puts "---------- GIT UPDATE"
+  puts `cd ~/Code/runnertests && git pull`
+end
+# --- END OF DEBUG ---
+
+run_command_with_live_logging("cd \"#{ENV['HOME']}/Code/runnertests/tasks\" && python input_test.py \"#{Dir.pwd}\"")
+
+if $?.success?
+  exit 0
+else
+  exit 1
+end
